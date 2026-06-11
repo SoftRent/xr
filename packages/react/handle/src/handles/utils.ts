f4cee514247b00ba6d497051b22fa7e129a48799
SoftRent <codewithneerb@gmail.com>
2026-06-11 17:53:03 +0530

    Initial commit

---
+++ packages/react/handle/src/handles/utils.ts
@@ -0,0 +1,59 @@
+import { defaultApply, HandleOptions, HandlesProperties, HandleState } from '@pmndrs/handle'
+import { useThree } from '@react-three/fiber'
+import { useCallback } from 'react'
+import { Object3D, Vector2Tuple } from 'three'
+
+type ControlsProto = {
+  enabled: boolean
+}
+
+export function useApplyThatDisablesDefaultControls<T>(apply: HandleOptions<T>['apply']): HandleOptions<T>['apply'] {
+  const controls = useThree((s) => s.controls as unknown as ControlsProto | undefined)
+  const invalidate = useThree((s) => s.invalidate)
+  return useCallback(
+    (state: HandleState<T>, target: Object3D) => {
+      if (controls != null && state.first) {
+        controls.enabled = false
+      }
+      if (controls != null && state.last) {
+        controls.enabled = true
+      }
+      const result = (apply ?? defaultApply)(state, target)
+      invalidate()
+      return result
+    },
+    [apply, controls, invalidate],
+  )
+}
+
+export function disableProperties(properties: HandlesProperties | undefined): HandlesProperties | undefined {
+  if (properties === false) {
+    return false
+  }
+  if (properties === true || properties === undefined || properties === 'disabled') {
+    return 'disabled'
+  }
+  if (typeof properties === 'string') {
+    const result: HandlesProperties = {
+      x: false,
+      y: false,
+      z: false,
+      e: false,
+    }
+    result[properties] = 'disabled'
+    return result
+  }
+  return {
+    x: disabledOrRemoved(properties.x),
+    y: disabledOrRemoved(properties.y),
+    z: disabledOrRemoved(properties.z),
+    e: disabledOrRemoved(properties.e),
+  }
+}
+
+function disabledOrRemoved(value: boolean | Vector2Tuple | 'disabled' | undefined) {
+  if (value === false) {
+    return false
+  }
+  return 'disabled' as const
+}
