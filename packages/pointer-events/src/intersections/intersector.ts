64760422505ecb6cfb5005f781ddd40ed3b37352
SoftRent <codewithneerb@gmail.com>
2026-06-11 17:52:28 +0530

    Initial commit

---
+++ packages/pointer-events/src/intersections/intersector.ts
@@ -0,0 +1,29 @@
+import { Mesh, Object3D, SphereGeometry } from 'three'
+import { Intersection } from '../index.js'
+import { PointerCapture } from '../pointer.js'
+
+const VoidObjectRadius = 10000000000
+const VoidObjectGeometry = new SphereGeometry(VoidObjectRadius)
+
+const sceneVoidObjectMap = new Map<Object3D, Object3D>()
+
+export function getVoidObject(scene: Object3D): Object3D {
+  let entry = sceneVoidObjectMap.get(scene)
+  if (entry == null) {
+    entry = new Mesh(VoidObjectGeometry)
+    entry.isVoidObject = true
+    entry.parent = scene
+    //makes sure all other intersections are always prioritized
+    entry.pointerEventsOrder = -Infinity
+    sceneVoidObjectMap.set(scene, entry)
+  }
+  return entry
+}
+
+export interface Intersector {
+  intersectPointerCapture(pointerCapture: PointerCapture, nativeEvent: unknown): Intersection
+  isReady(): boolean
+  startIntersection(nativeEvent: unknown): void
+  executeIntersection(scene: Object3D, objectPointerEventsOrder: number): void
+  finalizeIntersection(scene: Object3D): Intersection
+}
