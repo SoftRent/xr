67d208a2a2a27bdd3c5094cce9ceb74bf4a84cdc
SoftRent <codewithneerb@gmail.com>
2026-06-11 17:55:01 +0530

    Initial commit

---
+++ packages/xr/src/vanilla/space.ts
@@ -0,0 +1,26 @@
+import { Object3D } from 'three'
+import { createGetXRSpaceMatrix, getSpaceFromAncestors } from '../space.js'
+import { XRSpaceType } from './types.js'
+import { onXRFrame } from './utils.js'
+
+export class XRSpace extends Object3D {
+  constructor(
+    public readonly xrSpace: XRSpaceType,
+    origin?: Object3D,
+    originReferenceSpace?: XRReferenceSpace,
+  ) {
+    super()
+    this.transformReady = false
+    const getSpaceMatrix = createGetXRSpaceMatrix(xrSpace, () =>
+      getSpaceFromAncestors(this, origin, originReferenceSpace),
+    )
+    this.matrixAutoUpdate = false
+    this.visible = false
+    onXRFrame((frame) => {
+      if (frame == null) {
+        return
+      }
+      this.visible = this.transformReady = getSpaceMatrix(this.matrix, frame)
+    })
+  }
+}
