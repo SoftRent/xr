43ffedb19ec019859368a9712126eeffa42bba70
SoftRent <codewithneerb@gmail.com>
2026-06-11 17:51:30 +0530

    Initial commit

---
+++ packages/handle/src/handles/pivot/scale.ts
@@ -0,0 +1,49 @@
+import { ColorRepresentation, Mesh, MeshBasicMaterial, SphereGeometry } from 'three'
+import { Axis } from '../../state.js'
+import { HandlesContext } from '../context.js'
+import { HandlesProperties } from '../index.js'
+import { handleXRayMaterialProperties, setupHandlesContextHoverMaterial } from '../material.js'
+import { RegisteredHandle } from '../registered.js'
+import { extractHandleTransformOptions } from '../utils.js'
+
+export class PivotAxisScaleHandle extends RegisteredHandle {
+  constructor(context: HandlesContext, axis: Axis, tagPrefix: string) {
+    super(context, axis, tagPrefix, () => ({
+      scale: this.options,
+      rotate: false,
+      translate: 'as-scale',
+      multitouch: false,
+    }))
+  }
+
+  bind(defaultColor: ColorRepresentation, config?: HandlesProperties) {
+    const { options, disabled } = extractHandleTransformOptions(this.axis, config)
+    if (options === false) {
+      return undefined
+    }
+    this.options = options
+    const material = new MeshBasicMaterial(handleXRayMaterialProperties)
+    const cleanupHover = setupHandlesContextHoverMaterial(this.context, material, this.tag, {
+      color: defaultColor,
+      hoverColor: 0xffff40,
+      disabled,
+    })
+
+    const mesh = new Mesh(new SphereGeometry(0.04), material)
+    mesh.renderOrder = Infinity
+    mesh.pointerEventsOrder = Infinity
+    mesh.position.x = 0.68
+
+    const unregister = disabled ? undefined : this.context.registerHandle(this.store, mesh, this.tag)
+
+    this.add(mesh)
+
+    return () => {
+      material.dispose()
+      mesh.geometry.dispose()
+      unregister?.()
+      cleanupHover?.()
+      this.remove(mesh)
+    }
+  }
+}
