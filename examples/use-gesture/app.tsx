f6cd85e5d9f27ad26d5a7a3093c00ad7b4e1b933
SoftRent <codewithneerb@gmail.com>
2026-06-11 17:50:41 +0530

    Initial commit

---
+++ examples/use-gesture/app.tsx
@@ -0,0 +1,24 @@
+import { Canvas } from '@react-three/fiber'
+import { noEvents, PointerEvents } from '@react-three/xr'
+import { useDrag } from '@use-gesture/react'
+import { useRef } from 'react'
+
+export function App() {
+  return (
+    <Canvas style={{ width: '100%', flexGrow: 1 }} events={noEvents}>
+      <DragCube />
+      <PointerEvents />
+    </Canvas>
+  )
+}
+
+function DragCube() {
+  const ref = useRef<Mesh>(null)
+  const bind = useDrag(({ movement, xy, delta }) => console.log(...xy, 'movement', ...movement, 'delta', ...delta))
+  return (
+    <mesh {...(bind() as any)} position={[0, 1, -1]} scale={0.1}>
+      <boxGeometry />
+      <meshBasicMaterial color="blue" />
+    </mesh>
+  )
+}
