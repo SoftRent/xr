3e1e3dcf243b5ac918624579096961f314c75d2b
SoftRent <codewithneerb@gmail.com>
2026-06-11 17:54:10 +0530

    Initial commit

---
+++ packages/xr/src/controller/model.ts
@@ -0,0 +1,47 @@
+import { Material, Mesh, Object3D } from 'three'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { DefaultGltfLoader, XRControllerGamepadComponentId } from '../index.js'
+import { XRControllerLayout } from './layout.js'
+
+export async function loadXRControllerModel(layout: XRControllerLayout, loader: GLTFLoader = DefaultGltfLoader) {
+  const { scene } = await loader.loadAsync(layout.assetPath)
+  return scene.clone(true)
+}
+
+/**
+ * function for getting the object of a specific component from the xr controller model
+ */
+export function getXRControllerComponentObject(
+  model: Object3D,
+  layout: XRControllerLayout,
+  componentId: XRControllerGamepadComponentId,
+) {
+  const component = layout.components[componentId]
+  const firstVisualResponse = component.visualResponses[Object.keys(component.visualResponses)[0]]
+  if (firstVisualResponse == null) {
+    return undefined
+  }
+  return model.getObjectByName(firstVisualResponse.valueNodeName)
+}
+
+export type XRControllerModelOptions = {
+  /**
+   * allows to configure whether the controller is rendered to the color buffer
+   * can be used to show the real controller in AR passthrough mode
+   */
+  colorWrite?: boolean
+  /**
+   * allows to configure the render order of the controller model
+   * @default undefined
+   */
+  renderOrder?: number
+}
+
+export function configureXRControllerModel(model: Object3D, options?: XRControllerModelOptions) {
+  model.renderOrder = options?.renderOrder ?? 0
+  model.traverse((child) => {
+    if (child instanceof Mesh && child.material instanceof Material) {
+      child.material.colorWrite = options?.colorWrite ?? true
+    }
+  })
+}
