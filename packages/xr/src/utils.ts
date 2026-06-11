2ee5a8218aa8100fdcc0512b3074e19b56d6183d
SoftRent <codewithneerb@gmail.com>
2026-06-11 17:54:47 +0530

    Initial commit

---
+++ packages/xr/src/utils.ts
@@ -0,0 +1,26 @@
+import { Quaternion, Vector3 } from 'three'
+
+/**
+ * @param value is allowed to contain nan values
+ */
+export function toDOMPointInit(
+  value: Vector3 | Quaternion | undefined,
+  defaultW: number = 1,
+): DOMPointInit | undefined {
+  if (value == null) {
+    return undefined
+  }
+  return {
+    x: nanToDefault(value.x),
+    y: nanToDefault(value.y),
+    z: nanToDefault(value.z),
+    w: 'w' in value ? nanToDefault(value.w, defaultW) : defaultW,
+  }
+}
+
+export function nanToDefault(value: number, defaultValue: number = 0): number {
+  if (isNaN(value)) {
+    return defaultValue
+  }
+  return value
+}
