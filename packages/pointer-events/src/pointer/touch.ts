9a3274748fc73b020c8c284bee33c9e739ccad00
SoftRent <codewithneerb@gmail.com>
2026-06-11 17:52:42 +0530

    Initial commit

---
+++ packages/pointer-events/src/pointer/touch.ts
@@ -0,0 +1,68 @@
+import { Object3D } from 'three'
+import { Intersection, IntersectionOptions, SphereIntersector } from '../intersections/index.js'
+import { GetCamera, Pointer, PointerOptions } from '../pointer.js'
+import { generateUniquePointerId } from './index.js'
+
+export type TouchPointerOptions = {
+  /**
+   * @default 0.1
+   */
+  hoverRadius?: number
+  /**
+   * @default 0.03
+   */
+  downRadius?: number
+  /**
+   * @default 0
+   */
+  button?: number
+} & PointerOptions &
+  IntersectionOptions
+
+export function createTouchPointer(
+  getCamera: GetCamera,
+  space: { current?: Object3D | null },
+  pointerState: any,
+  options: TouchPointerOptions = {},
+  pointerType: string = 'touch',
+) {
+  return new Pointer(
+    generateUniquePointerId(),
+    pointerType,
+    pointerState,
+    new SphereIntersector(space, () => options.hoverRadius ?? 0.1, options),
+    getCamera,
+    createUpdateTouchPointer(options),
+    undefined,
+    undefined,
+    options,
+  )
+}
+
+function createUpdateTouchPointer(options: TouchPointerOptions) {
+  let wasPointerDown = false
+  return (pointer: Pointer) => {
+    if (!pointer.getEnabled()) {
+      return
+    }
+    const intersection = pointer.getIntersection()
+    const isPointerDown = computeIsPointerDown(intersection, options.downRadius ?? 0.03)
+    if (isPointerDown === wasPointerDown) {
+      return
+    }
+    const nativeEvent = { timeStamp: performance.now(), button: options.button ?? 0 }
+    if (isPointerDown) {
+      pointer.down(nativeEvent)
+    } else {
+      pointer.up(nativeEvent)
+    }
+    wasPointerDown = isPointerDown
+  }
+}
+
+function computeIsPointerDown(intersection: Intersection | undefined, downRadius: number): boolean {
+  if (intersection == null) {
+    return false
+  }
+  return intersection.distance <= downRadius
+}
