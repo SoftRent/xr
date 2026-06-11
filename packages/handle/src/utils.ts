7f927a7bb8a9205384970f805b51e22ddb166ba4
SoftRent <codewithneerb@gmail.com>
2026-06-11 17:52:09 +0530

    Initial commit

---
+++ packages/handle/src/utils.ts
@@ -0,0 +1,19 @@
+import { PointerEvent } from '@pmndrs/pointer-events'
+import { Vector3 } from 'three'
+
+export function getWorldDirection(event: PointerEvent, target: Vector3): boolean {
+  if (event.details.type === 'sphere') {
+    return false
+  }
+  if (event.details.type === 'lines') {
+    const { line } = event.details
+    target.copy(line.end).sub(line.start).normalize()
+    return true
+  }
+  if (event.details.type === 'screen-ray') {
+    target.copy(event.details.direction)
+    return true
+  }
+  target.set(0, 0, -1).applyQuaternion(event.pointerQuaternion)
+  return true
+}
