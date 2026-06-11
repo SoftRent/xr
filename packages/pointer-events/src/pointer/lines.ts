887b0f0405f4260f5fbe03acde8a0c4f71cc3160
SoftRent <codewithneerb@gmail.com>
2026-06-11 17:52:39 +0530

    Initial commit

---
+++ packages/pointer-events/src/pointer/lines.ts
@@ -0,0 +1,38 @@
+import { Object3D, Vector3 } from 'three'
+import { IntersectionOptions, LinesIntersector } from '../intersections/index.js'
+import { GetCamera, Pointer, PointerOptions } from '../pointer.js'
+import { generateUniquePointerId } from './index.js'
+
+export type LinesPointerOptions = {
+  /**
+   * @default 0
+   * distance to intersection in local space
+   */
+  minDistance?: number
+  /**
+   * points for that compose the lines
+   * @default [new Vector3(0,0,0), new Vector3(0,0,1)]
+   */
+  linePoints?: Array<Vector3>
+} & PointerOptions &
+  IntersectionOptions
+
+export function createLinesPointer(
+  getCamera: GetCamera,
+  space: { current?: Object3D | null },
+  pointerState: any,
+  options: LinesPointerOptions = {},
+  pointerType: string = 'lines',
+) {
+  return new Pointer(
+    generateUniquePointerId(),
+    pointerType,
+    pointerState,
+    new LinesIntersector(space, options),
+    getCamera,
+    undefined,
+    undefined,
+    undefined,
+    options,
+  )
+}
