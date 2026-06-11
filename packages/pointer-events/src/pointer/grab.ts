a85b2878e4f0f371d1f7308cf2acfe1a165cf3cb
SoftRent <codewithneerb@gmail.com>
2026-06-11 17:52:33 +0530

    Initial commit

---
+++ packages/pointer-events/src/pointer/grab.ts
@@ -0,0 +1,33 @@
+import { Object3D } from 'three'
+import { generateUniquePointerId } from './index.js'
+import { IntersectionOptions } from '../intersections/index.js'
+import { SphereIntersector } from '../intersections/sphere.js'
+import { GetCamera, Pointer, PointerOptions } from '../pointer.js'
+
+export type GrabPointerOptions = {
+  /**
+   * @default 0.07
+   */
+  radius?: number
+} & PointerOptions &
+  IntersectionOptions
+
+export function createGrabPointer(
+  getCamera: GetCamera,
+  space: { current?: Object3D | null },
+  pointerState: any,
+  options: GrabPointerOptions = {},
+  pointerType: string = 'grab',
+) {
+  return new Pointer(
+    generateUniquePointerId(),
+    pointerType,
+    pointerState,
+    new SphereIntersector(space, () => options.radius ?? 0.07, options),
+    getCamera,
+    undefined,
+    undefined,
+    undefined,
+    options,
+  )
+}
