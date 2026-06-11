1b9b8dff957aea3cd33b7f0796b18687ab52a12c
SoftRent <codewithneerb@gmail.com>
2026-06-11 17:50:22 +0530

    Initial commit

---
+++ examples/secondary-input-sources/vite.config.ts
@@ -0,0 +1,12 @@
+import basicSsl from '@vitejs/plugin-basic-ssl'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), basicSsl()],
+  base: '/xr/examples/secondary-input-sources/',
+  resolve: {
+    dedupe: ['@react-three/fiber', 'three'],
+  },
+})
