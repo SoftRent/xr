4355738a09df94cd82e53407d7dd38ae199770c9
SoftRent <codewithneerb@gmail.com>
2026-06-11 17:55:07 +0530

    Initial commit

---
+++ packages/xr/src/vanilla/xr.ts
@@ -0,0 +1,129 @@
+import { ForwardEventsOptions, forwardHtmlEvents, GetCamera } from '@pmndrs/pointer-events'
+import { Object3D, WebXRManager } from 'three'
+import { XRStore, XRStoreOptions, createXRStore as createXRStoreImpl } from '../store.js'
+import { setupSyncXRElements } from './elements.js'
+import {
+  DefaultXRControllerOptions,
+  DefaultXRGazeOptions,
+  DefaultXRHandOptions,
+  DefaultXRScreenInputOptions,
+  DefaultXRTransientPointerOptions,
+} from '../default.js'
+import type {
+  XRControllerState,
+  XRGazeState,
+  XRHandState,
+  XRScreenInputState,
+  XRTransientPointerState,
+} from '../input.js'
+
+export type XRElementImplementationCleanup = (() => void) | void
+
+export type XRUpdatesList = Array<(frame: XRFrame, delta: number) => void>
+
+export type XRElementImplementations = {
+  hand:
+    | ((
+        store: XRStore<XRElementImplementations>,
+        handSpace: Object3D,
+        state: XRHandState,
+        session: XRSession,
+      ) => XRElementImplementationCleanup)
+    | boolean
+    | DefaultXRHandOptions
+  gaze:
+    | ((
+        store: XRStore<XRElementImplementations>,
+        handSpace: Object3D,
+        state: XRGazeState,
+        session: XRSession,
+      ) => XRElementImplementationCleanup)
+    | boolean
+    | DefaultXRGazeOptions
+  screenInput:
+    | ((
+        store: XRStore<XRElementImplementations>,
+        handSpace: Object3D,
+        state: XRScreenInputState,
+        session: XRSession,
+      ) => XRElementImplementationCleanup)
+    | boolean
+    | DefaultXRScreenInputOptions
+  transientPointer:
+    | ((
+        store: XRStore<XRElementImplementations>,
+        handSpace: Object3D,
+        state: XRTransientPointerState,
+        session: XRSession,
+      ) => XRElementImplementationCleanup)
+    | boolean
+    | DefaultXRTransientPointerOptions
+  controller:
+    | ((
+        store: XRStore<XRElementImplementations>,
+        controllerSpace: Object3D,
+        state: XRControllerState,
+        session: XRSession,
+      ) => XRElementImplementationCleanup)
+    | boolean
+    | DefaultXRControllerOptions
+  detectedPlane:
+    | ((
+        store: XRStore<XRElementImplementations>,
+        detectedPlaneSpace: Object3D,
+        plane: XRPlane,
+        session: XRSession,
+      ) => XRElementImplementationCleanup)
+    | false
+  detectedMesh:
+    | ((
+        store: XRStore<XRElementImplementations>,
+        detecedMeshSpace: Object3D,
+        mesh: XRMesh,
+        session: XRSession,
+      ) => XRElementImplementationCleanup)
+    | false
+}
+
+export function createXRStore(
+  canvas: HTMLCanvasElement,
+  scene: Object3D,
+  getCamera: GetCamera,
+  xr: WebXRManager,
+  options?: XRStoreOptions<XRElementImplementations> & { htmlInput?: ForwardEventsOptions | false },
+) {
+  const htmlEventForward =
+    options?.htmlInput === false ? undefined : forwardHtmlEvents(canvas, getCamera, scene, options?.htmlInput)
+  const updatesList: XRUpdatesList = []
+  const store = createXRStoreImpl<XRElementImplementations>(options)
+  store.setWebXRManager(xr)
+  let cleanupSyncElements: (() => void) | undefined
+  const unsubscribeOrigin = store.subscribe((state, prevState) => {
+    if (state.origin === prevState.origin) {
+      return
+    }
+    cleanupSyncElements?.()
+    cleanupSyncElements =
+      state.origin != null ? setupSyncXRElements(scene, getCamera, store, state.origin, updatesList) : undefined
+  })
+  return Object.assign(store, {
+    destroy() {
+      unsubscribeOrigin()
+      htmlEventForward?.destroy()
+      cleanupSyncElements?.()
+      store.destroy()
+    },
+    update(frame: XRFrame | undefined, delta: number) {
+      htmlEventForward?.update()
+      if (frame == null) {
+        return
+      }
+      store.onBeforeFrame(scene, getCamera(), frame)
+      const length = updatesList.length
+      for (let i = 0; i < length; i++) {
+        updatesList[i](frame, delta)
+      }
+      store.onBeforeRender()
+    },
+  })
+}
