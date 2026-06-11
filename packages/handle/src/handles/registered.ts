73b53f11ba41b5b321f59e5a1e02937c8713bbdb
SoftRent <codewithneerb@gmail.com>
2026-06-11 17:51:28 +0530

    Initial commit

---
+++ packages/handle/src/handles/registered.ts
@@ -0,0 +1,21 @@
+import { Group } from 'three'
+import { HandleStore, HandleOptions } from '../store.js'
+import { HandlesContext } from './context.js'
+import { extractHandleTransformOptions } from './utils.js'
+
+export class RegisteredHandle extends Group {
+  public readonly store: HandleStore<unknown>
+  protected options: Exclude<ReturnType<typeof extractHandleTransformOptions>['options'], false> | undefined
+  protected readonly tag: string
+
+  constructor(
+    protected readonly context: HandlesContext,
+    protected readonly axis: 'x' | 'y' | 'z' | 'xy' | 'yz' | 'xz' | 'xyz' | 'e',
+    tagPrefix: string,
+    getOptions: () => HandleOptions<unknown>,
+  ) {
+    super()
+    this.tag = (tagPrefix ?? '') + axis
+    this.store = new HandleStore(context.target, () => context.getHandleOptions(this.tag, getOptions))
+  }
+}
